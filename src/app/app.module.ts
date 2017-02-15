b241dfff5aee1f556778c585d584b30d6f2c496c
Aleks Tsilyurik <ts.alex85@gmail.com>
2017-02-15 16:17:05 +0200

    change the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { SidebarLeftComponent } from './shared/sidebar-left/sidebar-left.compone
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
--- src/app/form-select/form-select.component.ts
+++ src/app/form-select/form-select.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form-select',
   templateUrl: './form-select.component.html',
   styleUrls: ['./form-select.component.css']
 })
-export class FormSelectComponent implements OnInit {
+export class FormSelectComponent{
+  matches = [1, 2, 3, 4, 5];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  selectForm = new FormGroup ({
+    numberOfMatches: new FormControl(),
+    result: new FormArray([
+      new FormControl('win'),
+      new FormControl('draw'),
+      new FormControl('loss')
+    ])
+  });
 }
